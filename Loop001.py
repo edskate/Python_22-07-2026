0ff79e812a557e7976560ae6bffb143a39eeae50
Edy_97_2024 <edskateicevida@gmail.com>
2026-07-22 23:00:22 -0300

    add .

---
+++ Loop001.py
@@ -0,0 +1,38 @@
+print("👉 Olá, mundo!")
+
+
+
+
+print("👉 Olá, Edy!")
+print("🐍 Estou aprendendo Python.")
+
+
+nome = "Edy"
+
+print(nome)
+print("👉 Meu nome é", nome)
+
+
+nome = input("Digite seu nome: ")
+
+print("👉 Olá,", nome)
+
+
+nome = input("Digite seu nome: ")
+
+print("👉 Olá,", nome)
+
+
+
+
+contador = 1
+
+while contador <= 5:
+    print("🇨🇲 Número:", contador)
+    contador = contador + 1
+    
+    
+    frutas = ["maçã", "banana", "uva"]
+
+for fruta in frutas:
+    print(fruta)
